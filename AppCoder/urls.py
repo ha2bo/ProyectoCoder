dff049060e4b770af47e9d5870efc220dc2e7ef2
samuel.guibin <samuel.gubin@gmail.com>
2024-06-03 21:08:04 -0500

    agregar templates y static

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -5,4 +5,9 @@ from AppCoder.views import *
 urlpatterns = [
     path('agregacurso/<nombre>/<camada>', curso),
     path('listcurso/', lista_curso),
+    path('', inicio),
+    path('cursos/', cursos),
+    path('profesores/', profesores),
+    path('estudiantes/', estudiantes),
+    path('entregables/', entregables),
 ]
